847b42c4aa8b7aabaea22d2237ad9754417f5527
Ticketmob <nasi@laughstub.com>
2012-04-24 13:25:20 -0700

    test

--- assets/js/app.js
+++ assets/js/app.js
@@ -55,23 +55,27 @@ run(function () {
     });
 
 	when('#shows', function () {
-		$('#showlist').html('Page is loading....');
-		$.ajax({
-			type: "GET",
-			url: "http://www.ticketmob.com/PhoneGap/index.cfm",
-			data: {
-				showID: '0'
-			},
-			dataType: "html",
-			success: function(result){
-				$('#showlist').html(result);
-			}
-		});
+		//$('#showlist').html('Page is loading....');
+		//$.ajax({
+		//	type: "GET",
+		//	url: "http://www.ticketmob.com/PhoneGap/index.cfm",
+		//	data: {
+		//		showID: '0'
+		//	},
+		//	dataType: "html",
+		//	success: function(result){
+		//		$('#showlist').html(result);
+		//	}
+		//});
 		
 		//$.mobile.changePage( "http://www.ticketmob.com/PhoneGap/index.cfm", { pageContainer: 'hidethisdiv' } );
 		//display('#shows');
 		//alert($('#showlist').html());
 		//$('#showlist').html('Overwrite this div');
+    });
+
+	when('#welcome1', function () {
+		display('#welcome');
     });
 
 });
