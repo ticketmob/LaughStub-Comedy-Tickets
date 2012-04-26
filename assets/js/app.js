87f2879bb4de653d77865c6b874261aae3e2870a
Ticketmob <nasi@laughstub.com>
2012-04-25 18:07:04 -0700

    test

--- assets/js/app.js
+++ assets/js/app.js
@@ -35,12 +35,12 @@ function runGeoLoc() {
 // 
 //  --- our app behavior logic ---
 //
-run(function () {
-    // immediately invoked on first run
-    var init = (function () {
-        if (navigator.network.connection.type == Connection.NONE) {
-            alert("No internet connection - we won't be able to show you any maps");
-        } else {
+run(function() {
+	// immediately invoked on first run
+	var init = (function () {
+		if (navigator.network.connection.type == Connection.NONE) {
+			alert("No internet connection - we won't be able to show you any maps");
+		} else {
             //alert("We can reach Google - get ready for some awesome maps!");
 			navigator.geolocation.getCurrentPosition(function (position) {
 				var coords = position.coords;
@@ -64,8 +64,8 @@ run(function () {
 			//	"home", "Home", "lsIcon.png", {"onSelect": function() {
 			//	}}
 			//);
-        }
-    })();
+		}
+    });
     
     // a little inline controller
     when('#welcome', function() {
