58ddf97430d8ebb251a00a32d2d276327a3b148e
Ticketmob <nasi@laughstub.com>
2012-04-26 16:21:00 -0700

    test

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,4 @@
 		var online = navigator.onLine || false;
-		var callbackFlag = false;
 
 		function getVenues() {
 			alert(online);
@@ -12,6 +11,23 @@
 		}
 
 		$(document).ready( function () {
+			navigator.geolocation.getCurrentPosition(function (position) {
+				var coords = position.coords;
+				var location = "" + position.coords.latitude + "," + position.coords.longitude;
+				var url = "http://maps.google.com/maps/api/staticmap?center=" + coords.latitude + "," + coords.longitude + "&zoom=13&size=320x480&maptype=roadmap&key=MyGoogleMapsAPIKey&sensor=true";
+	
+				/*
+				store.save( {
+					key: 'configLoc',
+					location: location,
+					lat: position.coords.latitude,
+					lon: position.coords.longitude
+				});
+				}, function () {
+					alert('Can\'t use your current position');
+				*/
+			});
+
 			$.ajax({
 				type: "GET",
 				url: "http://www.ticketmob.com/PhoneGap/index.cfm",
@@ -38,33 +54,12 @@
 			states[NetworkStatus.REACHABLE_VIA_CARRIER_DATA_NETWORK] = 'Carrier data connection';
 			states[NetworkStatus.REACHABLE_VIA_WIFI_NETWORK] = 'WiFi connection';
 			
-			if(networkState != 0) {
+			if(networkState != 0) 
 				online = true;
-				callbackFlag = true;
-			}
-
-			navigator.geolocation.getCurrentPosition(function (position) {
-				var coords = position.coords;
-				var location = "" + position.coords.latitude + "," + position.coords.longitude;
-				var url = "http://maps.google.com/maps/api/staticmap?center=" + coords.latitude + "," + coords.longitude + "&zoom=13&size=320x480&maptype=roadmap&key=MyGoogleMapsAPIKey&sensor=true";
-	
-				/*
-				store.save( {
-					key: 'configLoc',
-					location: location,
-					lat: position.coords.latitude,
-					lon: position.coords.longitude
-				});
-				}, function () {
-					alert('Can\'t use your current position');
-				*/
-			});
-
 		}
 		
 		// PhoneGap is loaded and it is now safe to make calls PhoneGap methods
 		function onDeviceReady() {
-			alert('device ready');
 			navigator.network.isReachable('google.com', reachableCallback, {});
 		}
 
